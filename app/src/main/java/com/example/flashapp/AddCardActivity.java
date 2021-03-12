7ce1d9343eb5cb7f5ce11baf0f2872bbea500c9d
oledibefrancis <somtofrancis380@gmail.com>
2021-03-12 00:35:11 -0600

    optional changes

---
+++ app/src/main/java/com/example/flashapp/AddCardActivity.java
@@ -0,0 +1,48 @@
+package com.example.flashapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+
+        ImageView CancleButton = findViewById(R.id.my_cancle_Btn);
+        ImageView SaveButton =findViewById(R.id.my_save_icon);
+        EditText editQuestion = findViewById(R.id.editQuestionField);
+        EditText editAnswer = findViewById(R.id.editAnswerField);
+
+
+
+        CancleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+//on click listener for the save button
+        SaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = editQuestion.getText().toString();
+                String answer = editAnswer.getText().toString();
+
+                Intent data =new Intent();     // create a new Intent, this is where we will put our data
+                data.putExtra("string1", question);  // puts one string into the Intent, with the key as 'string1'
+                data.putExtra("string2", answer);   // puts another string into the Intent, with the key as 'string2
+                setResult(RESULT_OK,data);  // set result code and bundle data for response
+                finish();    // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/flashapp/MainActivity.java
+++ app/src/main/java/com/example/flashapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.flashapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
+        ImageView addCard = findViewById(R.id.myBtn);
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+          addCard.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                  MainActivity.this.startActivityForResult(intent ,100);
+              }
+          });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 100 && resultCode==RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+            TextView view = (TextView)findViewById(R.id.flashcard_question);
+            view.setText(string1);
+            TextView view2 = (TextView)findViewById(R.id.flashcard_answer);
+            view2.setText(string2);
+
+        }
     }
 }
