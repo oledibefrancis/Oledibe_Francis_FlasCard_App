11312aafbf49844f5e1be3bfe3057df3ef1e06c2
oledibefrancis <somtofrancis380@gmail.com>
2021-03-17 23:39:32 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/flashapp/MainActivity.java
+++ app/src/main/java/com/example/flashapp/MainActivity.java
@@ -8,18 +8,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards; //a variable holding the list of flashcards
+    int currentCardDisplayedIndex = 0;//a variable to keep track of the index of the current card we're showing.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //declaration of variables
 
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
-        ImageView addCard = findViewById(R.id.myBtn);
+        ImageView Addcard = findViewById(R.id.plus_Btn);
+        ImageView OpenEye = findViewById(R.id.open_eye);
+        ImageView CloseEye = findViewById(R.id.close_eye);
+        ImageView NextCard = findViewById(R.id.next_card);
+        TextView answer1 = findViewById(R.id.answer_1);
+        TextView answer2 = findViewById(R.id.answer_2);
+        TextView answer3 = findViewById(R.id.answer_3);
+
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,14 +52,102 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-          addCard.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                  MainActivity.this.startActivityForResult(intent ,100);
-              }
-          });
-    }
+
+
+
+//The statement below sets an onclick listener for the answer options
+        answer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer1.setBackgroundColor(getResources().getColor(R.color.green, null));
+            }
+        });
+
+        answer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer2.setBackgroundColor(getResources().getColor(R.color.red, null));
+            }
+        });
+
+        answer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer3.setBackgroundColor(getResources().getColor(R.color.red, null));
+            }
+        });
+
+       //The statement below sets an onclick listener for the Open eye buttons
+        OpenEye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenEye.setVisibility(View.INVISIBLE);
+                CloseEye.setVisibility(View.VISIBLE);
+                answer1.setVisibility(View.VISIBLE);
+                answer2.setVisibility(View.VISIBLE);
+                answer3.setVisibility(View.VISIBLE);
+                answer1.setBackgroundColor(getResources().getColor(R.color.brown, null));
+                answer2.setBackgroundColor(getResources().getColor(R.color.brown, null));
+                answer3.setBackgroundColor(getResources().getColor(R.color.brown, null));
+            }
+        });
+
+
+        //The statement below sets an onclick listener for the Closed eye buttons
+        CloseEye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenEye.setVisibility(View.VISIBLE);
+                CloseEye.setVisibility(View.INVISIBLE);
+                answer1.setVisibility(View.INVISIBLE);
+                answer2.setVisibility(View.INVISIBLE);
+                answer3.setVisibility(View.INVISIBLE);
+            }
+        });
+            //This statement sets an onclick listner for the next card button
+        NextCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards.size() == 0)
+                    return;
+                currentCardDisplayedIndex++;// advance our pointer index so we can show the next card
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Toast.makeText(getApplicationContext(), "You've reached the end of the cards, going back to start.", Toast.LENGTH_SHORT).show();
+                    currentCardDisplayedIndex = 0;
+                }
+                // set the question and answer TextViews with data from the database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getAnswer());
+            }
+        });
+
+
+
+//The statement below sets an on click listener for the plus button
+        Addcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext()); //initializing the flashcard variable
+        allFlashcards = flashcardDatabase.getAllCards();// variable initialization, we can access all flash cards
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer()); }
+        }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
@@ -54,7 +158,12 @@ public class MainActivity extends AppCompatActivity {
             view.setText(string1);
             TextView view2 = (TextView)findViewById(R.id.flashcard_answer);
             view2.setText(string2);
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
 
         }
     }
+
+
+
 }
