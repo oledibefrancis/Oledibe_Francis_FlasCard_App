e09d1bb6ec3d6a70f2f4c1806ef9912a4559ac72
oledibefrancis <somtofrancis380@gmail.com>
2021-03-31 23:25:43 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/flashapp/MainActivity.java
+++ app/src/main/java/com/example/flashapp/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.flashapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.plattysoft.leonids.ParticleSystem;
 
 import java.util.List;
 
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     FlashcardDatabase flashcardDatabase;
     List<Flashcard> allFlashcards; //a variable holding the list of flashcards
     int currentCardDisplayedIndex = 0;//a variable to keep track of the index of the current card we're showing.
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +46,34 @@ public class MainActivity extends AppCompatActivity {
         TextView answer3 = findViewById(R.id.answer_3);
 
 
+
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 questionTextView.setVisibility(View.INVISIBLE);
                 answerTextView.setVisibility(View.VISIBLE);
+
+                View answerSideView = findViewById(R.id.flashcard_answer);
+                View questionSideView = findViewById(R.id.flashcard_answer);
+
+
+                // get the center for the clipping circle
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+
+// get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+// create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
+
+// hide the question and show the answer to prepare for playing the animation!
+                questionSideView.setVisibility(View.INVISIBLE);
+                answerSideView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(3000);
+                anim.start();
+
             }
         });
         answerTextView.setOnClickListener(new View.OnClickListener() {
@@ -57,11 +87,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
 //The statement below sets an onclick listener for the answer options
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 answer1.setBackgroundColor(getResources().getColor(R.color.green, null));
+                new ParticleSystem(MainActivity.this, 100, R.drawable.confetti, 3000)
+                        .setSpeedRange(0.2f, 0.6f)
+                        .oneShot(findViewById(R.id.answer_1), 100);
             }
         });
 
@@ -78,6 +112,23 @@ public class MainActivity extends AppCompatActivity {
                 answer3.setBackgroundColor(getResources().getColor(R.color.red, null));
             }
         });
+
+
+
+//        // The block of code below is to implement a coundown timer
+//        countDownTimer = new CountDownTimer(16000, 1000) {
+//            public void onTick(long millisUntilFinished) {
+//                ((TextView) findViewById(R.id.timer)).setText("" + millisUntilFinished / 1000);
+//            }
+//
+//            public void onFinish() {
+//            }
+//        };
+////
+//        private void startTimer() {
+//            countDownTimer.cancel();
+//            countDownTimer.start();
+//        }
 
        //The statement below sets an onclick listener for the Open eye buttons
         OpenEye.setOnClickListener(new View.OnClickListener() {
@@ -128,6 +179,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
@@ -153,8 +205,34 @@ public class MainActivity extends AppCompatActivity {
 
                 ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getQuestion());
                 ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getAnswer());
+
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.right_in);
+
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+                findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
+                findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
             }
         });
+
+
+
+
 
         flashcardDatabase = new FlashcardDatabase(getApplicationContext()); //initializing the flashcard variable
         allFlashcards = flashcardDatabase.getAllCards();// variable initialization, we can access all flash cards
